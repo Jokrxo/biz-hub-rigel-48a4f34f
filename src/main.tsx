839478dc416f275bee182049eec43aecc02ea85d
Rigel Business <rigel@example.com>
2025-12-12 15:00:30 +0200

    PWA: stop unregistering SW in dev; restore install prompt

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,3 @@ import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
-if (import.meta.env.DEV && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => {
-    regs.forEach(reg => reg.unregister());
-  }).catch(() => {});
-}
